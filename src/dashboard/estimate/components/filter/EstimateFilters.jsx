95d654c7b7cd0fd3820cd5c90d81e6e65991a3c4
Alejo Ruiz <alejoruizs@hotmail.com>
2023-06-05 23:24:43 -0500

    Add estimate filters

---
+++ src/dashboard/estimate/components/filter/EstimateFilters.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react"
+import moment from 'moment'
+import { Filter } from "../../../../components/filter"
+import { InputLabelContainer, Label } from "../../../../components/form"
+import { SelectClient } from "../../../clients/components"
+import { DATA_PICKER_FORMAT, USA_DATE_FORMAT } from "../../../../utils/constants"
+import { DateError } from "../../../../components/filter/errors"
+
+export const EstimateFilters = ({estimatePrices, setestimatePrices, setShowAllRows, prepareEstimatedPrices}) => {
+  const today = moment()
+  const lastMonth = moment().startOf('month')
+  const [endDateValue, setEndDateValue] = useState(today.format(DATA_PICKER_FORMAT))
+  const [startDate, setStartDate] = useState(lastMonth.format(DATA_PICKER_FORMAT))
+  const [clientValue, setClientValue] = useState("")
+  const [errorMsn, setErrorMsn] = useState("")
+
+  const onClick = () => {
+    let estimatesFiltered = estimatePrices
+    if(startDate && endDateValue && startDate > endDateValue) {
+      setErrorMsn(<DateError />)
+      setShowAllRows(false)
+    }else {
+      setErrorMsn(<></>)
+      if(startDate){
+        const startDateFormatted = moment(startDate).format(USA_DATE_FORMAT)
+        estimatesFiltered = estimatesFiltered.filter(estimatePrice => estimatePrice.createdAt >= startDateFormatted )
+      }
+      if(endDateValue){
+        const endDateFormatted = moment(endDateValue).format(USA_DATE_FORMAT)
+        estimatesFiltered = estimatesFiltered.filter(estimatePrice => estimatePrice.createdAt <= endDateFormatted)
+      }
+      if(clientValue){
+        estimatesFiltered = estimatesFiltered.filter(estimatePrice => estimatePrice.clientName === clientValue)
+      }
+      setShowAllRows(true)
+    }
+    setestimatePrices(prepareEstimatedPrices(estimatesFiltered))
+  }
+
+  return (
+    <Filter onClick={onClick} errorMsn={errorMsn}>
+      <div className="w-1/5">
+        <InputLabelContainer
+          type="date"
+          text="Start date"
+          name="start_date"
+          css="flex flex-col text-center px-2"
+          inputValue={startDate}
+          setInputValue={setStartDate}
+        />
+      </div>
+      <div className="w-1/5">
+        <InputLabelContainer
+          type="date"
+          text="End date"
+          name="end_date"
+          css="flex flex-col text-center px-2"
+          inputValue={endDateValue}
+          setInputValue={setEndDateValue}
+        />
+      </div>
+      <div className="w-1/5 flex flex-col text-center">
+        <Label htmlFor="client" text="Client" />
+        <SelectClient name="clients" client={clientValue} setClient={setClientValue} />
+      </div>
+    </Filter>
+  )
+}
--- src/dashboard/estimate/pages/EstimatePage.jsx
+++ src/dashboard/estimate/pages/EstimatePage.jsx
@@ -6,6 +6,7 @@ import { ESTIMATED_PRICES } from '../../../utils/constants'
 import { getEstimatedPrices } from '../../../api/estimatedPrice/estimatedPrice'
 import { formatCurrency } from '../../../utils/functions'
 import { NotItemsFound } from '../../components/NotItemsFound/NotItemsFound'
+import { EstimateFilters } from '../components/filter/EstimateFilters'
 
 const thList = ['Created at', 'Name', 'Client Name', 'Price', 'Golden Price']
 const rowsToShow = ['id', 'createdAt', 'name', 'clientName', 'totalPrice', 'goldenPrice']
@@ -22,6 +23,7 @@ const prepareEstimatedPrices = (estimatePrices) => {
 export const EstimatePage = () => {
   const estimatedPricesInSS = JSON.parse(sessionStorage.getItem(ESTIMATED_PRICES) || '[]')
   const [estimatePrices, setestimatePrices] = useState(prepareEstimatedPrices(estimatedPricesInSS))
+  const [showAllRows, setShowAllRows] = useState(false)
 
   useEffect(() => {
     if(!estimatePrices.length) {
@@ -36,6 +38,7 @@ export const EstimatePage = () => {
         <h2 className="text-3xl text-center my-2">
           ESTIMATES
         </h2>
+        <EstimateFilters estimatePrices={estimatedPricesInSS} setestimatePrices={setestimatePrices} setShowAllRows={setShowAllRows} prepareEstimatedPrices={prepareEstimatedPrices} />
         <div className="px-4 py-3 flex justify-around sm:px-6">
           <Link
             className="border-2 p-2 rounded-lg border-blue-700 bg-blue-700 text-white w-[300px] hover:bg-blue-800 hover:shadow-md text-center"
@@ -49,7 +52,7 @@ export const EstimatePage = () => {
             <div className="overflow-hidden">
               {
                 estimatePrices.length
-                ? <Table thList={thList} tdList={estimatePrices} rowsToShow={rowsToShow} route="estimate" showAllRows={false} />
+                ? <Table thList={thList} tdList={estimatePrices} rowsToShow={rowsToShow} route="estimate" showAllRows={showAllRows} />
                 : <NotItemsFound text="estimates" />
               }
             </div>
